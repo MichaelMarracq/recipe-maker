89ce8a2eceeca5e033431bbbdd0d73232c3c54fd
LilMike707 <michaelmarracq@yahoo.com>
2023-11-02 14:51:57 -0700

    Update server.js

--- recipe-maker/src/server.js
+++ recipe-maker/src/server.js
@@ -36,4 +36,53 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/favorite", async (req, res) => {
+  const { userID, recipeId } = req.body;
+
+  // Insert the favorite into the "favorites" table
+  const query = {
+    text: "INSERT INTO favorites(user_id, recipe_id) VALUES($1, $2) RETURNING *",
+    values: [userID, recipeId],
+  };
+
+  try {
+    const result = await pool.query(query);
+
+    if (result.rowCount === 1) {
+      res.status(201).json({ message: "Favorite saved successfully" });
+    } else {
+      res.status(500).json({ message: "Saving favorite failed" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Saving favorite failed" });
+  }
+});
+
+router.get("/api/getfavorites", async (req, res) => {
+  try {
+    const { userID } = req.query; // Assuming userID is sent as a query parameter
+    if (!userID) {
+      return res.status(400).json({ message: "Missing userID" });
+    }
+
+    // Query your database to retrieve favorite recipe IDs associated with the userID
+    const favoriteRecipes = await db.getFavoritesByUserID(userID);
+
+    if (favoriteRecipes.length === 0) {
+      return res.status(404).json({ message: "No favorite recipes found" });
+    }
+
+    // Extract recipe IDs from the database response
+    const favoriteRecipeIDs = favoriteRecipes.map((recipe) => recipe.recipeID);
+
+    return res.status(200).json(favoriteRecipeIDs);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Error retrieving favorite recipes" });
+  }
+});
+
 module.exports = router;
